0ed3c60eca03162fc27bf17c929a856c4dfe26f7
lucyimhoff <lucyi4@nycstudents.net>
2024-09-10 23:56:42 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double a = 2.5;
+        int b = 7;
+        System.out.println(a * b * Math.PI);
+        String c = "Code works: ";
+        boolean d = true;
+        System.out.println(c + d);
+        b = b * 2;
+        String e = "b is now: ";
+        System.out.println(e + b);
+
+    }
+}
